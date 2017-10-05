2d371c7673aa37441e1dc898c9919f0070b98826
Thanh Vo <thanhvo@Thanhs-MBP.lan>
2017-10-05 11:44:18 +0700

    Game interface

---
+++ Assets/Source/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void mainmenu() {
+		Application.LoadLevel("Main");
+	}
+
+	public void startgame() {
+		Application.LoadLevel("Startgame");
+	}
+
+	public void howtoplay() {
+		Application.LoadLevel("Howtoplay");
+	}
+
+	public void about() {
+		Application.LoadLevel("About");
+	}
+
+	public void exit() {
+		Application.Quit();
+	}
+}
